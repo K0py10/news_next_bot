0e804a2c5227197d93c62877f945020718ca4d2b
K0py10 <122783778+K0py10@users.noreply.github.com>
2023-06-06 16:33:30 +0300

    Added some channel  actions

--- bot&parser.py
+++ bot&parser.py
@@ -18,110 +18,99 @@ from emoji import replace_emoji
 
 
 greeting_text = "Hello. \nI'm a bot, designed to unite all channels' posts in one place.\nTo get commands list, type /help"
-help_text = "Commands List: \n /add_channel - Add channel to list of channels, whose posts will be fetched (Thereafter – list of channels)"
-channel_added_text = "Channel added successfully"
-channel_adding_error_text = "Something went wrong, check your input and try again"
+help_text = "Commands List: \n /subscribe_to [channel] — Add channel to list of channels, whose posts will be fetched (Thereafter — list of channels) \n/unsubscribe_from [channel] — delete channel from list of channels"
+channel_added_text = "Channel added successfully."
+wrong_channel_error_text = "Something went wrong, check your input and try again."
+adding_channel_internal_error_text = "Something went wrong on our side, you may try again."
+already_subscribed_text = "You are already subscribed to this channel."
+successfully_unsubscribed_text = "You successfully unsubscribed from this channel."
+didnt_unsubscribe_text = "You were not subscribed to this channel. So, well, nothing really happened."
+no_subscribed_channels_text = "you are not subscribed to any channels.\nUse /subscribe_to command to subscribe to new channel"
 
 async def start_CR(update: Update, context: ContextTypes.DEFAULT_TYPE): #response to /start command
     await context.bot.send_message(update.effective_chat.id, greeting_text) 
 async def help_CR(update: Update, context: ContextTypes.DEFAULT_TYPE): #response to /help command
 	await context.bot.send_message(update.effective_chat.id, help_text)       
-async def add_channel_CR(update: Update, context: ContextTypes.DEFAULT_TYPE): #response to /add_channel command
-    if await add_channel(update.message.text.replace("/add_channel ", ""), update.effective_chat.id):
-        await context.bot.send_message(update.effective_chat.id, channel_added_text) 
-    else:
-        await context.bot.send_message(update.effective_chat.id, channel_adding_error_text) 
-async def retrieve_messages_CR(update: Update, context: ContextTypes.DEFAULT_TYPE): #, last_message_id
-    channels_list = db.get_all_channels(cur)
-    print("channels list: " + str(channels_list))
-    for ch in channels_list:
-        last_id = 0
-        print('channel' + str(ch))
-        async for msg in parser.iter_messages(ch.name, min_id = ch.last_message, reverse = True): #iterate over new channel messages
-            last_id = msg.id
-            try:
-                if msg.raw_text != None and msg.raw_text != '': #sort out technical messages
-                    cleaned_text = replace_emoji(msg.raw_text.replace("\n", " "), replace = '')
-                    lem_text = sh.prepare_text(cleaned_text)
-
-                    entity = await parser.get_entity(ch.name)
-                    text_to_send = entity.title + ":\n" +  msg.text + "\nhttps://t.me/" + entity.username + "/" + str(msg.id)
-
-                    similar_posts = sh.find_similar_posts(lem_text, db.get_chats_posts(cur, [update.effective_chat.id]), cutoff)
-                    if similar_posts == []:
-                        post_db_id = db.save_post(cur, ch.id, ' '.join(lem_text))
-                        message = await context.bot.send_message(update.effective_chat.id, text = text_to_send, entities= [MessageEntity('BOLD', 1, len(entity.title) + 1)], parse_mode='HTML', disable_web_page_preview=True)
-                        db.add_post_to_chat(cur, update.effective_chat.id, message.id, text_to_send, post_db_id)
-                    else:
-                        message = db.get_chat_post(cur, update.effective_chat.id, similar_posts[-1])
-                        text_to_send = message.text + "\nhttps://t.me/" + entity.username + "/" + msg.id
-                        bot.edit_message_text(text_to_send, update.effective_chat.id, message.id, parse_mode = 'HTML', disable_web_page_preview = True)
-                        db.update_chat_message(cur, message, text_to_send) 
-            except Exception as e:
-                print("Parsing message failed\nException: ", e, "\nText:", msg.text, "\nID", msg.id)
-        if last_id != 0:    
-            db.update_last_message(cur, ch.id, last_id)
-
-async def add_channel(channel_user_input, chat_id): #
+async def add_channel_CR(update: Update, context: ContextTypes.DEFAULT_TYPE): #response to /subscribe_to command
     try:
-        entity = await parser.get_entity(channel_user_input)
+        entity = await parser.get_entity(update.message.text.replace("/subscribe_to", ""))
         name = entity.username  
         #getting telegram's internal channel entity (used to verify thet the link is valid and unify stored data)
         channel_lastmessage = -1 #used for handling errors
         channel_lastmessage = await get_channel_lm(name)
         if channel_lastmessage != -1:
-            db.save_channel(cur, name, channel_lastmessage - randint(1, 10), chat_id)
-            return True
+            if db.save_channel(name, channel_lastmessage - randint(1, 10), update.effective_chat.id):
+                await context.bot.send_message(update.effective_chat.id, channel_added_text) 
+            else:
+                await context.bot.send_message(update.effective_chat.id, already_subscribed_text) 
         else:
-            return False
-    except Exception as e: 
-        print("error: " + str(e))
-        return False
+            await context.bot.send_message(update.effective_chat.id, wrong_channel_error_text)
+    except Exception as e:
+        print("Error while getting channel: ", e)
+        await context.bot.send_message(update.effective_chat.id, adding_channel_internal_error_text)
+async def delete_channel_CR(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    entity = await parser.get_entity(update.message.text.replace("/unsubscribe_from", ""))
+    ch_name = entity.username
+    if db.unsubscribe(ch_name, update.effective_chat.id):
+        await context.bot.send_message(update.effective_chat.id, successfully_unsubscribed_text)
+    else:
+        await context.bot.send_message(update.effective_chat.id, didnt_unsubscribe_text)
+async def my_channels_CR(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_channels = db.get_user_channels(update.effective_chat.id)
+    if user_channels != []:
+        text_to_send = 'List of channels that you are subscribed to: '
+        for ch in user_channels:
+            text_to_send += '\n' + ch
+            await context.bot.send_message(update.effective_chat.id, user_channels)
+    else:
+        await context.bot.send_message(update.effective_chat.id, no_subscribed_channels_text)
+    
+
 async def get_channel_lm(channel_username):
     channel_lastmessage = -1
     async for message in parser.iter_messages(channel_username, limit = 1): #getting last message from channel
         channel_lastmessage = message.id #saving its id
     return channel_lastmessage
 
-async def update():
-    channels_list = db.get_all_channels(cur)
+async def update(update : Update, context : ContextTypes.DEFAULT_TYPE):
+    channels_list = db.get_all_channels()
     for ch in channels_list:
-        if get_channel_lm(ch.name) != ch.last_message: #if there are new messages
-            dest_list = db.get_channel_subs(cur, ch.id)
+        if await get_channel_lm(ch.name) != ch.last_message: #if there are new messages
+            dest_list = db.get_channel_subs(ch.id)
             last_id = 0
             async for msg in parser.iter_messages(ch.name, min_id = ch.last_message, reverse=True):
-                try:
+                # try:
                     last_id = msg.id
                     if msg.text != None and msg.text != '': #sort out technical & mediagroup messages
                         cleaned_text = replace_emoji(msg.raw_text.replace("\n", " "), replace = '')
                         lem_text = sh.prepare_text(cleaned_text)
-                        sim_posts = sh.find_similar_posts(lem_text, db.get_chats_posts(cur, dest_list), cutoff)
+                        sim_posts = sh.find_similar_posts(lem_text, db.get_chats_posts(dest_list), cutoff)
                         sim_ids = [sim_post.id for sim_post in sim_posts]
                         entity = await parser.get_entity(ch.name)
                         text_to_send = entity.title + ":\n" +  msg.text + "\nhttps://t.me/" + entity.username + "/" + str(msg.id)
                         for sub in dest_list:
-                            try:
-                                sim_msgs_in_chat = set(sim_ids) & set(db.get_chat_post_ids(cur, sub))
+                            # try:
+                                sim_msgs_in_chat = set(sim_ids) & set(db.get_chat_post_ids(sub))
                                 if sim_msgs_in_chat == set():
-                                    post_db_id = db.save_post(cur, ch.id, lem_text)
+                                    post_db_id = db.save_post(ch.id, ' '.join(lem_text))
                                     message = await bot.send_message(sub, text = text_to_send, parse_mode='HTML')
-                                    db.add_post_to_chat(update.effective_chat.id, message.id, text_to_send, post_db_id)
+                                    db.add_post_to_chat(sub, message.id, text_to_send, post_db_id)
                                 else:
-                                    message = db.get_chat_post(cur, sub, max(sim_msgs_in_chat))
-                                    text_to_send = message.text + "\nhttps://t.me/" + entity.username + "/" + msg.id
-                                    bot.edit_message_text(text_to_send, sub, message.id, parse_mode = 'HTML', disable_web_page_preview = True)
-                                    db.update_chat_message(cur, message, text_to_send) 
-                            except Exception as e:
-                                print("Parsing message failed\nException: ", e, "\nChat_id:", sub, "\nChannel: ", ch[1], "\nID: ", msg.id)
-                except Exception as e:
-                    print("Parsing message failed\nException: ", e, "\nChat_id:", sub, "\nChannel: ", ch[1], "\nID: ", msg.id)
+                                    for sim_msg in sim_msgs_in_chat:
+                                        message = db.get_chat_post(sub, sim_msg)
+                                        text_to_send = message.text + "\nhttps://t.me/" + entity.username + "/" + str(msg.id)
+                                        bot.edit_message_text(text_to_send, sub, message.id, parse_mode = 'HTML', disable_web_page_preview = True)
+                                        db.update_chat_message(message, text_to_send) 
+                #             except Exception as e:
+                #                 print("Parsing message failed\nException: ", e, "\nChat_id:", sub, "\nChannel: ", ch.name, "\nID: ", msg.id)
+                # except Exception as e:
+                #     print("Parsing message failed\nException: ", e, "\nChat_id:", sub, "\nChannel: ", ch[1], "\nID: ", msg.id)
             if last_id != 0:
-                db.update_last_message(cur, ch.id, last_id)
-    time.sleep(1)
+                db.update_last_message(ch.id, last_id)
 
-async def run_bot():
-    second_loop = asyncio.new_event_loop()
-    loop.run_until_complete(application.run_polling())
+# async def run_bot():
+#     second_loop = asyncio.new_event_loop()
+#     loop.run_until_complete(application.run_polling())
 
 async def run_updater():
     while True:
@@ -131,7 +120,6 @@ async def run_updater():
             await asyncio.sleep(1)
         except Exception as e:
             print("TELETHON ERROR: " + str(e))
-
 async def run_bot():
     await application.initialize()
     await application.updater.start_polling(poll_interval = 1, timeout = 1)
@@ -152,7 +140,7 @@ async def run_bot():
         except Exception as e:
             print("PTB ERROR: " + str(e))
 
-    # await application.updater.stop()
+    await application.updater.stop()
     await application.stop()
     await application.shutdown()
 
@@ -164,25 +152,27 @@ if __name__ == "__main__":
         bot_token = data["bot_token"]
     parser = TelegramClient('anon', api_id, api_hash).start()
     parser.parse_mode = 'html'
-    ApplicationBuilder.concurrent_updates = True
+    # ApplicationBuilder.concurrent_updates = True
     application = ApplicationBuilder().token(bot_token).build()
     bot = application.bot
-    con = sqlite3.connect('db.sql')
-    cur = con.cursor()
     cutoff = 0.4
     update_offset = 0
-    db.restart_db(cur)
+    # db.restart_db()
 
     application.add_handler(CommandHandler('start', start_CR))
     application.add_handler(CommandHandler('help', help_CR))
-    application.add_handler(CommandHandler('add_channel', add_channel_CR))   
-    application.add_handler(CommandHandler('retrieve_messages', retrieve_messages_CR)) 
+    application.add_handler(CommandHandler('subscribe_to', add_channel_CR))  
+    application.add_handler(CommandHandler('unsubscribe_from', delete_channel_CR)) 
+    application.add_handler(CommandHandler('retrieve_messages', update)) 
+    application.add_handler(CommandHandler('my_channels', my_channels_CR))
 
-    loop = asyncio.get_event_loop()
-    loop.create_task(run_bot())
-    loop.create_task(run_updater())
-    loop.run_forever()
+    # loop = asyncio.get_event_loop()
+    # loop.create_task(run_updater())
+    # loop.create_task(run_bot())
+    # loop.run_forever()
     
+    application.run_polling()
+
     # bot_process = Process(target = application.run_polling())
     # bot_process.start()
     # bot_process.join()
@@ -209,3 +199,35 @@ if __name__ == "__main__":
     # application.updater.stop()
     # await application.stop()
     # await application.shutdown()
+
+    
+# async def retrieve_messages_CR(update: Update, context: ContextTypes.DEFAULT_TYPE): #, last_message_id
+    # channels_list = db.get_all_channels()
+    # print("channels list: " + str(channels_list))
+    # for ch in channels_list:
+    #     last_id = 0
+    #     print('channel' + str(ch))
+    #     async for msg in parser.iter_messages(ch.name, min_id = ch.last_message, reverse = True): #iterate over new channel messages
+    #         last_id = msg.id
+    #         try:
+    #             if msg.raw_text != None and msg.raw_text != '': #sort out technical messages
+    #                 cleaned_text = replace_emoji(msg.raw_text.replace("\n", " "), replace = '')
+    #                 lem_text = sh.prepare_text(cleaned_text)
+
+    #                 entity = await parser.get_entity(ch.name)
+    #                 text_to_send = entity.title + ":\n" +  msg.text + "\nhttps://t.me/" + entity.username + "/" + str(msg.id)
+
+    #                 similar_posts = sh.find_similar_posts(lem_text, db.get_chats_posts([update.effective_chat.id]), cutoff)
+    #                 if similar_posts == []:
+    #                     post_db_id = db.save_post(, ch.id, ' '.join(lem_text))
+    #                     message = await context.bot.send_message(update.effective_chat.id, text = text_to_send, entities= [MessageEntity('BOLD', 1, len(entity.title) + 1)], parse_mode='HTML', disable_web_page_preview=True)
+    #                     db.add_post_to_chat(update.effective_chat.id, message.id, text_to_send, post_db_id)
+    #                 else:
+    #                     message = db.get_chat_post(update.effective_chat.id, similar_posts[-1])
+    #                     text_to_send = message.text + "\nhttps://t.me/" + entity.username + "/" + msg.id
+    #                     bot.edit_message_text(text_to_send, update.effective_chat.id, message.id, parse_mode = 'HTML', disable_web_page_preview = True)
+    #                     db.update_chat_message(message, text_to_send) 
+    #         except Exception as e:
+    #             print("Parsing message failed\nException: ", e, "\nText:", msg.text, "\nID", msg.id)
+    #     if last_id != 0:    
+    #         db.update_last_message(ch.id, last_id)
--- db_handler.py
+++ db_handler.py
@@ -1,47 +1,66 @@
 import sqlite3
 import classes
 
-def restart_db(cur):
+con = sqlite3.connect('db.sql')
+cur = con.cursor()
+
+def restart_db():
     cur.execute("DROP TABLE IF EXISTS channels")
     cur.execute("CREATE TABLE channels(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, last_message INTEGER NOT NULL)")
     cur.execute("DROP TABLE IF EXISTS subs")
-    cur.execute("CREATE TABLE subs(chat_id INTEGER NOT NULL, channel INTEGER NOT NULL REFERENCES channels(id))")
+    cur.execute("CREATE TABLE subs(chat INTEGER NOT NULL, channel INTEGER NOT NULL REFERENCES channels(id))")
     cur.execute("DROP TABLE IF EXISTS posts")
     cur.execute("CREATE TABLE posts (id INTEGER PRIMARY KEY AUTOINCREMENT, channel INTEGER NOT NULL REFERENCES channels(id), lem_text TEXT NOT NULL)")
     cur.execute("DROP TABLE IF EXISTS chat_posts")
     cur.execute("CREATE TABLE chat_posts (chat INTEGER NOT NULL, id INTEGER NOT NULL, text TEXT NOT NULL, post INTEGER NOT NULL REFERENCES posts(chat_id))")
     cur.connection.commit()
 
-def save_channel(cur, name, last_message, chat_id):
+def save_channel(name, last_message, chat_id):
     channel_id = cur.execute("SELECT id FROM channels WHERE name = ? LIMIT 1", (name,)).fetchmany() 
     if channel_id == []: #adding channel to channels table if it doesn't exist
         channel = {'name' : name, 'last_message' : last_message}
         cur.execute("INSERT INTO channels (name, last_message) VALUES (:name, :last_message)", channel)
         channel_id = cur.execute("SELECT id FROM channels ORDER BY id DESC LIMIT 1").fetchmany()
-    
-    cur.execute("INSERT INTO subs (chat_id, channel) VALUES (:chat_id, :channel)", {'chat_id': chat_id, 'channel': channel_id[0][0]}) #adding subscription
-    cur.connection.commit()
+    if cur.execute("SELECT * FROM subs WHERE chat = :chat AND channel = :channel_id", {'chat' : chat_id, "channel_id" : channel_id[0][0]}).fetchmany() == []: #checking if user is already subscribed
+        cur.execute("INSERT INTO subs (chat, channel) VALUES (:chat_id, :channel)", {'chat_id': chat_id, 'channel': channel_id[0][0]}) #adding subscription
+        cur.connection.commit()
+        return True
+    else:
+        cur.connection.commit()
+        return False
 
-def get_all_channels(cur):
+def unsubscribe(channel, chat):
+    channel_id = cur.execute("SELECT id FROM channels WHERE name = ?", (channel,)).fetchmany()
+    if channel_id != []:
+        if cur.execute("SELECT * FROM subs WHERE chat = :chat AND channel = :channel", {'chat': chat, 'channel': channel_id[0][0]}).fetchmany() != []: #checking if user is already subscribed
+            cur.execute("DELETE FROM subs WHERE chat = :chat AND channel = :channel", {'chat': chat, 'channel': channel_id[0][0]})
+            con.commit()
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def get_all_channels():
     res = []
     for row in cur.execute("SELECT * FROM channels"):
         res.append(classes.channel(row[0], row[1], row[2]))
         #print(row)
     return res
 
-def update_last_message(cur, id, last_message):
+def update_last_message(id, last_message):
     cur.execute("UPDATE channels SET last_message = ? WHERE id = ?", (last_message, id))
     cur.connection.commit()
 
-def get_channel_subs(cur, channel_id):
+def get_channel_subs(channel_id):
     res = []
-    for row in cur.execute("SELECT subs.chat_id FROM channels INNER JOIN subs ON channels.id = subs.channel WHERE channel_id = ?", (channel_id,)):
+    for row in cur.execute("SELECT subs.chat FROM channels INNER JOIN subs ON channels.id = subs.channel WHERE channel = ?", (channel_id,)):
         res.append(row[0])
     return res
 
 
-def save_post(cur, channel_id, lem_text):
-    same_post_id = cur.execute("SELECT id FROM posts WHERE lem_text = ?", (lem_text, )).fetchmany()
+def save_post(channel_id, lem_text):
+    same_post_id = cur.execute("SELECT id FROM posts WHERE lem_text = ?", (lem_text,)).fetchmany()
     if same_post_id == []:
         cur.execute("INSERT INTO posts (channel, lem_text) VALUES (:channel_id, :lem_lext)", {"channel_id": channel_id, "lem_lext": lem_text})
         cur.connection.commit()
@@ -49,23 +68,23 @@ def save_post(cur, channel_id, lem_text):
     else:
         return same_post_id[0][0]
 
-def get_all_posts(cur):
+def get_all_posts():
     res = []
     for row in cur.execute("SELECT * FROM posts"):
         res.append(classes.post(row[0], row[1], row[2]))
     return res
 
-def add_post_to_chat(cur, chat, id, text, posts_id):
+def add_post_to_chat(chat, id, text, posts_id):
     cur.execute("INSERT INTO chat_posts (chat, id, text, post) VALUES (:chat, :id, :text, :post)", {"chat": chat, "id": id, "text": text, "post": posts_id})
     cur.connection.commit()
 
-def get_chat_post_ids(cur, chat_id):
+def get_chat_post_ids(chat_id):
     res = []
-    for row in cur.execute("SELECT posts.id FROM chat_posts INNER JOIN posts ON chat_posts.post = posts.id WHERE chat_posts.chat_id = ?", (chat_id)).fetchall():
+    for row in cur.execute("SELECT post FROM chat_posts WHERE chat = ?", (chat_id,)).fetchall():
         res.append(row[0])
     return res
 
-def get_chats_posts(cur, chats):
+def get_chats_posts(chats):
     res = []
     check_ids = []
     ch_chats = '(' + str(chats[0])
@@ -78,12 +97,17 @@ def get_chats_posts(cur, chats):
             check_ids.append(row[0])
     return res
 
-def get_chat_post(cur, chat, post_id):
-    res = []
+def get_chat_post(chat, post_id):
     for row in cur.execute("SELECT id, text FROM chat_posts WHERE chat = ? AND post = ?", (chat, post_id)):
-        res.append(classes.message(chat, row[0], row[1]))
+        res = classes.message(chat, row[0], row[1])
     return res
 
-def update_chat_message(cur, message, text):
+def get_user_channels(user):
+    res = []
+    for row in cur.execute("SELECT channels.name FROM subs INNER JOIN channels ON channels.id = subs.channel WHERE subs.chat = ?", (user,)):
+        res.append(row[0])
+    return res
+
+def update_chat_message(message, text):
     cur.execute("UPDATE chat_posts SET text = :text WHERE chat = :chat AND id = :id", {"text": text, "chat": message.chat, "id": message.id})
     cur.commit()
